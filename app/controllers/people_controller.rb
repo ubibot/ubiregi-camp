820fb0e0bc6bb8489afd56c90f08bbe94c78ab33
Soutaro Matsumoto <matsumoto@soutaro.com>
2015-06-17 00:43:26 +0900

    debug print in people_controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,7 +5,6 @@ class PeopleController < ApplicationController
 
   def show
     @person = client.person(params[:id])
-    pp @person.attributes
     @projects = client.projects.sort_by(&:name)
     @person_projects = Set.new(@person.projects.map(&:id))
   end
